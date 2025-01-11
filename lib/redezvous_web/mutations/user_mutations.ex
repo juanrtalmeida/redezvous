1ae0116a911dacd2f5d3b40578436a11b2176a4f
Juan Almeida <juan.almeida@cwi.com.br>
2025-01-11 20:18:47 -0300

    chore: format

--- lib/redezvous_web/mutations/user_mutations.ex
+++ lib/redezvous_web/mutations/user_mutations.ex
@@ -7,10 +7,10 @@ defmodule RedezvousWeb.Mutations.UserMutations do
   object :user_mutations do
     @desc "register a new user"
     field :register_user, :user do
-      arg :name, non_null(:string)
-      arg :email, non_null(:string)
-      arg :password, non_null(:string)
-      resolve &Redezvous.create_new_user/2
+      arg(:name, non_null(:string))
+      arg(:email, non_null(:string))
+      arg(:password, non_null(:string))
+      resolve(&Redezvous.create_new_user/2)
     end
   end
 end
